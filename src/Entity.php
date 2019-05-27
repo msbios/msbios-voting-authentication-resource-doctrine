77283639fa890e7c9021728796b5da73fc9e64b9
Judzhin Miles <info@msbios.com>
2019-05-27 16:12:33 +0300

    Lazy Composer MSBios Commit Script

--- src/Entity.php
+++ src/Entity.php
@@ -6,13 +6,16 @@
 
 namespace MSBios\Voting\Authentication\Resource\Doctrine;
 
+use Doctrine\ORM\Mapping as ORM;
 use MSBios\Resource\Doctrine\Entity as DefaultEntity;
+use MSBios\Resource\Doctrine\IdentifierAwareTrait;
 
 /**
  * Class Entity
  * @package MSBios\Voting\Authentication\Resource\Doctrine
+ * @ORM\MappedSuperclass
  */
-class Entity extends DefaultEntity
+abstract class Entity extends DefaultEntity
 {
-    // ...
+    use IdentifierAwareTrait;
 }
